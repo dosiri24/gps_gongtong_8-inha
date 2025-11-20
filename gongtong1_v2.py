a90b4208d7b98b9e281e5b88ec61d1ec0b2b82de
dosiri24 <dosiri24@gmail.com>
2025-11-20 14:21:45 +0900

    avg, dx, dy, dz 계산

--- gongtong1_v2.py
+++ gongtong1_v2.py
@@ -1,5 +1,6 @@
+import math
 
-def get_gps_from_nmea(file_path):
+def get_gps_from_nmea(file_path): # [위도, N or S, 경도, E or W, 고도, 해발고도]
     nmedata = []
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
@@ -18,14 +19,79 @@ def get_gps_from_nmea(file_path):
                 
     return nmedata
     
+def latlong2xyz(lati, longi, alti):
+    lati_rad = math.radians(lati)
+    longi_rad = math.radians(longi)
+    a = 6378137  #(m) WGS84 장축 반경
+    e = 0.0818191908426215 #WGS84 이심률
+    b = a * math.sqrt(1 - e**2) #(m) WGS84 단축 반경
+    
+    N = (a**2) / math.sqrt(((a**2)*(math.cos(lati_rad))) + (b**2)* (math.sin(lati_rad))**2) #묘유선 곡률반경
+    
+    x = (N + alti) * math.cos(lati_rad) * math.cos(longi_rad)
+    y = (N + alti) * math.cos(lati_rad) * math.sin(longi_rad)
+    z = (((b**2)/(a**2))*N + alti) * math.sin(lati_rad)
+    
+    return x, y, z
+
+def cal_avg_and_dxdydz(corner_data):
+    dxdydz = []
+    xxs, yys, zzs = [], [], [] 
+    
+    for data in corner_data:
+        lati = data[0]
+        longi = data[2]
+        alti = data[4] + data[5]
+        x, y, z = latlong2xyz(lati, longi, alti)
+        xxs.append(x)
+        yys.append(y)
+        zzs.append(z)
+        
+    avg_x = sum(xxs) / len(xxs)
+    avg_y = sum(yys) / len(yys)
+    avg_z = sum(zzs) / len(zzs)
+    
+    for i in range(len(xxs)):
+        dx = xxs[i] - avg_x
+        dy = yys[i] - avg_y
+        dz = zzs[i] - avg_z
+        
+        dxdydz.append([dx, dy, dz])
+        
+    return [avg_x, avg_y, avg_z], [xxs, yys, zzs], dxdydz
+
+def save_csv(receiver_type, corner_num, avg_xyz, all_xyz, dxdydz):
+    filename = "output.csv"
+    
+    with open(filename, 'a', encoding='utf-8') as file:
+        for i in range(len(all_xyz[0])):
+            dx, dy, dz = dxdydz[i]
+            index = i
+            file.write(f"{receiver_type},{corner_num},{index},{all_xyz[0][i]},{all_xyz[1][i]},{all_xyz[2][i]},{dx},{dy},{dz}\n")
+        index = 'Avg'
+        file.write(f"{receiver_type},{corner_num},{index},{avg_xyz[0]},{avg_xyz[1]},{avg_xyz[2]},0,0,0\n")
+        
+    
 if __name__ == "__main__":
     phone_corners = {1: [], 2: [], 3: [], 4: []} #[꼭짓점번호][gngga데이터리스트][내부값]]
     rtk_corners = {1: [], 2: [], 3: [], 4: []}  #[꼭짓점번호][gngga데이터리스트][내부값]]
+    corners = 4
     
-    for i in range(1, 5):
+    with open("output.csv", 'w', encoding='utf-8') as f:
+        f.write("Receiver, CornerNum, Index, X, Y, Z, Dx, Dy, Dz\n")
+    
+    for i in range(1, 1+corners):
         phone_file_path = f"phone_data/phone{i}.txt"
         rtk_file_path = f"rtk_data/rtk{i}.txt"
         phone_corners[i] = (get_gps_from_nmea(phone_file_path))
         rtk_corners[i] = (get_gps_from_nmea(rtk_file_path))
         
-    print(phone_corners[1][1])
+    for i in range(1, 1+corners):     
+        avg_phone_xyz, all_phone_xyz, phone_dxdydz = cal_avg_and_dxdydz(phone_corners[i])
+        save_csv("Phone", i, avg_phone_xyz, all_phone_xyz, phone_dxdydz)
+    
+    for i in range(1, 1+corners):     
+        avg_rtk_xyz, all_rtk_xyz, rtk_dxdydz = cal_avg_and_dxdydz(rtk_corners[i])
+        save_csv("RTK", i, avg_rtk_xyz, all_rtk_xyz, rtk_dxdydz)
+        
+            
